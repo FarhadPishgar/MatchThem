a0d86926a37ff8283fc8cb9bc8d04fc0d31efa4e
Noah Greifer <noah.greifer@gmail.com>
2021-02-22 18:12:57 -0500

    Added note to self about components of objects

--- R/mimids.R
+++ R/mimids.R
@@ -23,3 +23,8 @@
 #' @keywords classes
 
 NULL
+
+#object: original mids object augmented with estimated weights
+#models: weightit objects
+#datasets: imputed datasets with estimated weights (lapply(0:m, .(i) complete(W$object, i)))
+#others: list with source, which is the original mids object
